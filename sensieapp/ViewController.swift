f8043be30a63b3e60e5062199f4873b8548b6183
Calil-Silva <ka.renner@hotmail.com>
2022-02-24 08:21:53 -0300

    Initial Commit

---
+++ sensieapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sensieapp
+//
+//  Created by Calil Renner Silva on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
